c833c419ae6810965c1829d326df0b04fa00a79e
DaviFaustino <davifaustino.dev@gmail.com>
2025-01-29 18:29:20 -0300

    test: create integration test for the report card endpoints

---
+++ src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_users
+VALUES ('teacher', '$2a$10$1bflKZk1XjdBl9Nlj9/bMetIIXzIu4MF3jK4DjgOg.WKGqt3WNQRS', 'TEACHER'),
+('student', '$2a$10$LP/hwTclnQ4bUy3lFdEQLusgOQhBjO/71kSEyYDnk8adHS2jwSBO6', 'STUDENT');
+
+INSERT INTO tb_report_cards
+VALUES ('0238ab71-7ead-4621-a962-62101e9df158', 'teacher', 'HISTORY', 'student', '2024', ARRAY['8.00', '8.00', '8.00', '8.00']);
